7e65f65f71fb5c0cef6fd7637ce922b9e6b377ae
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2023-04-15 15:40:26 -0300

    Adicionar arquivos de projeto.

---
+++ csharpBank/ContaBancaria.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpBank
+{
+    public class ContaBancaria
+    {
+        public long NumeroDaConta { get; private set; }
+        public string NomeTitular { get; private set; }
+        public double ContaSaldo { get; private set; }
+
+        public static long QuantidadeDeContas { get; private set;}
+        public ContaBancaria(string nomeTitular, double contaSaldo)
+        {
+            NumeroDaConta++;
+            QuantidadeDeContas++;
+            NomeTitular = nomeTitular;
+            ContaSaldo = contaSaldo;
+        }
+        public string DepositarValor(double valor)
+        {
+            if (valor < 0)
+                throw new ArgumentException("O valor depositado não pode ser negativo");
+            else
+                ContaSaldo += valor;
+                return "Valor depositado: R$"+valor;
+        }
+
+        public string SacarValor(double valor)
+        {
+            ContaSaldo -= valor;
+            return "Valor sacado: R$"+valor;
+        }
+
+        public string TransferirValor(double valor, ContaBancaria contaDestino)
+        {
+            if (valor < 0)
+                throw new ArgumentException("O valor transferido não pode ser negativo!");
+            else
+                ContaSaldo -= valor;
+                contaDestino.DepositarValor(valor);
+            return "Valor R$" + valor + " transferido para conta " + contaDestino.NumeroDaConta + " com sucesso!";
+        }
+    }
+}
